3749ec7ad6486a539127b48d35ad804195912999
Capotasto <rocker519804@gmail.com>
2016-02-09 22:15:17 -0800

    fixed some bug

--- rpgoop/ViewController.swift
+++ rpgoop/ViewController.swift
@@ -22,16 +22,56 @@ class ViewController: UIViewController {
     
     var player: Player!
     var enemy: Enemy!
-    
+    var chestMessage: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        player = Player(name
+        player = Player(name: "DirtyLaundry21", hp: 110, attackPwr: 20)
+        
+        generateRandomEnemy()
+        
+        playerHpLbl.text = "\(player.hp) HP"
+    }
+    
+    func generateRandomEnemy(){
+        let rand = Int(arc4random_uniform(2))
+        if rand == 0 {
+            enemy = Kimara(startingHp: 50, attackPwr: 12)
+        } else {
+            enemy = DevilWizard(startingHp: 60, attackPwr: 15)
+        }
+        enemyHpLbl.text = "\(String(enemy.hp)) HP"
+        enemyImg.hidden = false
     }
 
     @IBAction func onChestTapped(sender: AnyObject) {
+        chestBtn.hidden = true
+        printLbl.text = chestMessage
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "generateRandomEnemy", userInfo: nil, repeats: false)
     }
     
+    @IBAction func attackTapped(sender: AnyObject) {
+        if enemy.attemptAttack(player.attackPwr){
+            printLbl.text = "Attacked \(enemy.type) for \(player.attackPwr) HP"
+            enemyHpLbl.text = "\(enemy.hp) HP"
+            
+        }else {
+            printLbl.text = "Attack was unsuccessfule!"
+        }
+        
+        if let loot = enemy.dropLoot(){
+            player.additemToInventory(loot)
+            chestMessage = "\(player.name) found \(loot)"
+            chestBtn.hidden = false
+        }
+        
+        if !enemy.isAlive{
+            enemyHpLbl.text = ""
+            printLbl.text = "Killed \(enemy.type)"
+            enemyImg.hidden = true
+        }
+    
+    }
 
 
 }
